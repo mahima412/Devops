c4d2213ab962e7fc18115a7eef41448017d20840
mahima412 <mahimabajwa62@gmail.com>
2021-12-29 18:43:51 +0530

    Adding Devops Project to Git

---
+++ src/main/java/Devops/Devops/App.java
@@ -0,0 +1,13 @@
+package Devops.Devops;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Devops Learning" );
+    }
+}
